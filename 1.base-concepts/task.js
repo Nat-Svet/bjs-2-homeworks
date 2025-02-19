dfffce6cb1b83723cbc261e0853bb09531aa0195
Natalia Svetlitskaya <svetlitskaya73@gmail.com>
2025-02-19 21:22:47 +0300

    Реализовано задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+   let d = (b ** 2) - (4 * a * c);
+  if (d < 0) {
+    return [];
+  } else if (d === 0) {
+    return [-b / (2 * a)];
+  } else {
+    return [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)]
+  }
+  }
+ 
+  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+    let p = (percent / 100) / 12;
+    let n = countMonths;
+    let s = amount - contribution;
+    let paymentMonthly = s * (p + (p / (((1 + p) ** n) - 1)));
+    let amountTotal = paymentMonthly * n;
+    return +amountTotal.toFixed(2);
+  }
+   
+ 
   
-  return arr;
-}
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+
+
+
+
